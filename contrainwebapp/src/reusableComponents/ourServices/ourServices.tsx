e24478e9f24aa4b02c2466c358c3e00f94abf44c
Anna Özmehak <anna.ozmehak@gmail.com>
2025-03-18 10:59:35 +0100

    created ourServices component and styled it

---
+++ contrainwebapp/src/reusableComponents/ourServices/ourServices.tsx
@@ -0,0 +1,43 @@
+'use client';
+import useIsMobile from '@/hooks/useIsMobile';
+import styles from './ourServices.module.css';
+
+interface ServiceItem {
+    id: number;
+    Text: string;
+    Icon?: string | null;
+    Url: string;
+    Image?: { url: string } | null;
+  }
+
+interface OurServicesProps {
+    Title: string;
+    SubText?: string;
+    Services: ServiceItem[];
+}
+
+const OurServicesComponent: React.FC<OurServicesProps> = ({ Title, SubText, Services = [] }) => {
+    const isMobile = useIsMobile(); // Detect if mobile    
+
+    return (
+        <div className={styles.ourServicesContainer}>
+            <div className={styles.ourServicesHeader}>
+                <h4>{Title}</h4>
+                {SubText && <p>{SubText}</p>}
+            </div>
+            <div className={styles.serviceContainer}>
+                {Services.map((service) => (
+                    <a href={service.Url} key={service.id} className={styles.serviceCard}>
+                        <img src={service.Image?.url || "/microcontroller.webp"} alt="service" className={styles.serviceImage} />
+                        <div className={styles.serviceTextContent}>
+                            {service.Icon && <img src={service.Icon} alt="icon" />}
+                            <h5>{service.Text}</h5>
+                        </div>
+                    </a>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default OurServicesComponent;
